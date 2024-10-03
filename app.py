95077db00badec1e26b93030a0c514ae88ce0fff
Afshin <iafshinafshin@gmail.com>
2024-10-03 23:26:25 +0330

    feat(app): show ip

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from time import sleep
 from os import system
+from subprocess import call
 
-
-seconds = int(input("What do yo want change your IP? just write number [1,3600]: "))
+sleepTime = int(input("What do yo want change your IP? just write number [1,3600]: "))
 
 
 def enable():
@@ -13,14 +13,14 @@ def changeIP():
     while True:
         system("systemctl restart tor.service")
         # system('echo "IP: $(curl --socks5-hostname localhost:9050 https://api.ipify.org; echo)"')
-        system(
-            'echo "IP : $(curl --socks5-hostname localhost:9050 https://api.ipify.org; echo)"'
-        )
-        sleep(seconds)
+        # system(
+        #     'echo "IP : $(curl --socks5-hostname localhost:9050 https://api.ipify.org; echo)"'
+        # )
+        system('echo "IP : $(torsocks wget -qO - https://api.ipify.org; echo)"')
+        sleep(sleepTime)
 
 
 def main():
-    enable()
     changeIP()
 
 
